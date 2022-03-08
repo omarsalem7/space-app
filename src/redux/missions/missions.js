272d1dbb1cdca28aa4f26182a5e0a99b116b3d34
omarsalem7 <omarsalem0721@gmail.com>
2022-03-08 10:08:57 +0200

    setup reduc and dependancies

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,4 @@
+// write your actions creator and reducers here
+// This is a fake reducer replace body of it with your code (josphat)
+const missionsReducer = () => 'hi from fake reducer for missions';
+export default missionsReducer;

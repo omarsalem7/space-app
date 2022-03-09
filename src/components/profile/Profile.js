38191159f0af43306315e33af8cf88530acb4376
omarsalem7 <omarsalem0721@gmail.com>
2022-03-09 02:36:21 +0200

    add rocket in profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { ListGroup } from 'react-bootstrap/';
+import './profile.css';
 
-const Profile = () => (
-  <div>
-    profile
-  </div>
-);
-
+const Profile = () => {
+  const reserved = useSelector(({ rocketsReducer }) => rocketsReducer.filter((ro) => ro.reserved));
+  return (
+    <div className="profile">
+      <div className="missions">
+        <ListGroup>
+          <ListGroup.Item>fake data</ListGroup.Item>
+          <ListGroup.Item>fake data</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div className="rockets">
+        <ListGroup>
+          {reserved.map((rocket) => (
+            <ListGroup.Item key={rocket.id}>{rocket.rocketName}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
+    </div>
+  );
+};
 export default Profile;
